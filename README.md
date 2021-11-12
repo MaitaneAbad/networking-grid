bb94a36457a0a19ece4e6ca9244e9cca36557f70
MaitaneAbad <maitaneabadconde@gmail.com>
2021-11-12 20:58:20 +0100

    readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 ## Read started kit
+
+<a href="https://www.codefactor.io/repository/github/maitaneabad/networking-grid"><img src="https://www.codefactor.io/repository/github/maitaneabad/networking-grid/badge" alt="CodeFactor" /></a>
+https://www.codefactor.io/repository/github/maitaneabad/networking-grid/badge
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,11 @@
-// import '../styles/layout/header.scss';
+import '../styles/layout/footer.scss';
 
 const Footer = () => {
-  return <div className='footer'>&:Maitane</div>;
+  return (
+    <div className='footer'>
+      <p className='footer__name'> &copy; Maitane Abad Conde - 2021 </p>
+      <p className='footer__subtitle'> Hackaton - jobMadrid</p>
+    </div>
+  );
 };
 export default Footer;
